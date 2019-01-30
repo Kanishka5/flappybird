d224f6099ebce6ae3f62370c3c54ca9920f312da
battulasaivinesh <vineshbattula@gmail.com>
2019-01-31 00:32:07 +0530

    add Auth

--- js/game.js
+++ js/game.js
@@ -243,17 +243,27 @@
               fill: "#ffffff",
               align: "center"
             };
-            window.alert(window.email);
+            axios({
+              method: "put",
+              url:
+                "https://reg.arhn.co.in/api/flappybird/edit_email/" +
+                "kanishkan14@gmail.com" +
+                "/",
+              data: {
+                email: "kanishkan14@gmail.com",
+                name: "",
+                score: this.score
+              },
+              auth: {
+                username: "wdct",
+                password: "Wdct@cca"
+              }
+            }).catch(error => {
+              console.error(error);
+            });
+
             this.background = this.game.add.sprite(0, 0, "background");
-            if (window.email) {
-              this.email = this.game.add.text(
-                this.game.world.centerX,
-                200,
-                window.email,
-                { font: "45px Arial", fill: "#ffffff", align: "center" }
-              );
-              this.email.anchor.setTo(0.5, 0.5);
-            }
+
             this.ground = this.game.add.tileSprite(
               0,
               400 * 3,
